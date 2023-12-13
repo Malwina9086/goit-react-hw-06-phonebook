b38c175de5a637a4205e3b1c057059ee2bc1e2fc
Malwina9086 <malwina.lukjanska1@gmail.com>
2023-12-13 22:06:04 +0000

    last changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,15 @@
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import React from 'react';
-import { useSelector } from 'react-redux';
 
 const App = () => {
-  const contacts = useSelector(state => state.contacts);
-
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
-      <ContactList contacts={contacts} />
+      <ContactList />
     </div>
   );
 };
